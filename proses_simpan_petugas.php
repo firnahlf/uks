f44a9bbc2f08ba1af3723c3ad14dfbd4f65ece7d
Heri Risnanto <sparta.nibel@gmail.com>
2018-05-18 04:12:37 +0700

    Revert "Simpan Petugas Baru Fixed"
    This reverts commit ee8db68a32b7f1cc95a03564a64cda258dc9c121.

--- proses_simpan_petugas.php
+++ proses_simpan_petugas.php
@@ -11,23 +11,20 @@ $level = $_POST['level'];
 if($username =="" or $password =="")
 {
   ?>
-  <script language="javascript">
+  <script language="javascript">document.location.href="home.php?halaman=15";
       alert("Masih ada form yang kosong!");
-      document.location.href="home.php?halaman=15";
   </script>
 <?php
 
 }
 
 else {
-  $query = mysqli_query($koneksi, "INSERT INTO login (id_user, username, password, level) VALUES ('id_user', '$username', '$password', '$level')");
+  $query = mysqli_query($koneksi, "INSERT INTO login (username, password, level) VALUES ('id_user', '$username','$password','$level')");
 
   if($query)
   {
       ?>
-          <script language="javascript">
-            alert("Data petugas baru berhasil disimpan!");
-            document.location.href="home.php?halaman=15";
+          <script language="javascript">document.location.href="home.php?halaman=15"; alert("Data petugas baru berhasil disimpan!");
           </script>
       <?php
   }
@@ -36,9 +33,7 @@ else {
 
   {
       ?>
-          <script language="javascript">
-            alert("Data petugas baru gagal disimpan!");
-            document.location.href="home.php?halaman=15";
+          <script language="javascript">document.location.href="home.php?halaman=15"; alert("Data petugas baru gagal disimpan!");
           </script>
       <?php
   }
