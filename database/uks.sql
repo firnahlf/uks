271c398de172cda11e029d99fb1310f347a84d1f
Firna Helfira <firna.helfira@gmail.com>
2018-05-18 04:16:50 +0700

    upload projectuks

--- database/uks.sql
+++ database/uks.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
--- http://www.phpmyadmin.net
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 23, 2018 at 08:17 AM
--- Server version: 5.6.24
--- PHP Version: 5.6.8
+-- Host: localhost
+-- Generation Time: May 17, 2018 at 11:10 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.1.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `uks`
@@ -23,16 +25,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `data_kunjungan`
+--
+
+CREATE TABLE `data_kunjungan` (
+  `id_kunjungan` int(50) NOT NULL,
+  `id_pasien` int(50) NOT NULL,
+  `tgl_kunjungan` varchar(50) NOT NULL,
+  `nama_pasien` varchar(50) NOT NULL,
+  `status_pasien` varchar(50) NOT NULL,
+  `jk_pasien` varchar(50) NOT NULL,
+  `keluhan` varchar(50) NOT NULL,
+  `tindakan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `data_obat`
 --
 
-CREATE TABLE IF NOT EXISTS `data_obat` (
+CREATE TABLE `data_obat` (
   `id_obat` int(11) NOT NULL,
   `detail_obat` varchar(100) NOT NULL,
   `nama_obat` varchar(100) NOT NULL,
   `jml_obat` int(11) NOT NULL,
   `satuan_obat` varchar(20) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `data_obat`
@@ -47,7 +66,7 @@ INSERT INTO `data_obat` (`id_obat`, `detail_obat`, `nama_obat`, `jml_obat`, `sat
 -- Table structure for table `data_pasien`
 --
 
-CREATE TABLE IF NOT EXISTS `data_pasien` (
+CREATE TABLE `data_pasien` (
   `id_pasien` int(20) NOT NULL,
   `nama_pasien` varchar(100) NOT NULL,
   `tempat_lahir_pasien` varchar(50) NOT NULL,
@@ -57,20 +76,18 @@ CREATE TABLE IF NOT EXISTS `data_pasien` (
   `jk_pasien` varchar(10) NOT NULL,
   `no_telp` int(12) NOT NULL,
   `status_pasien` varchar(20) NOT NULL,
-  `foto_pasien` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `foto_pasien` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `data_pasien`
 --
 
 INSERT INTO `data_pasien` (`id_pasien`, `nama_pasien`, `tempat_lahir_pasien`, `tgl_lahir_pasien`, `alamat_pasien`, `agama_pasien`, `jk_pasien`, `no_telp`, `status_pasien`, `foto_pasien`) VALUES
-(1, 'Hafiz Alifiarga', 'Jakarta', '1994-10-18', 'Depok', 'Islam', 'Pria', 2147483647, 'Guru', ''),
-(2, 'Hafiz Marshall', 'Jakarta', '1994-10-18', 'Depok', 'Islam', 'Pria', 2147483647, 'Peserta Didik', ''),
-(3, 'Hafiz Airlangga', 'jakarta', '1994-10-18', 'depok', 'Islam', 'Pria', 2147483647, 'Guru', ''),
-(4, 'Hafiz Alinaga', 'jakarta', '1994-10-18', 'depok', 'Islam', 'Pria', 98798787, 'Peserta Didik', ''),
-(5, 'Hafiz Aliando', 'jakarta', '1994-10-18', 'depok', 'Islam', 'Pria', 85787587, 'Karyawan', ''),
-(6, 'Hafiz Bagus', 'jakarta', '1994-10-18', 'Depok', 'Islam', 'Pria', 654654654, 'Karyawan', '');
+(10001, 'firna ', 'cilegon', '0000-00-00', 'cilegon', 'Islam', 'Wanita', 2147483647, 'Peserta Didik', ''),
+(10002, 'noni', 'padang', '0000-00-00', 'padang', 'Islam', 'Wanita', 2147483647, 'Peserta Didik', ''),
+(10003, 'nadia', 'depok', '0000-00-00', 'depok', 'Islam', 'Wanita', 86678965, 'Peserta Didik', 'mbuntu-5.jpg'),
+(10004, 'heri', 'jakarta', '0000-00-00', 'mampang, Jakarta ', 'Islam', 'Pria', 867564321, 'Peserta Didik', 'mbuntu-8.jpg');
 
 -- --------------------------------------------------------
 
@@ -78,7 +95,7 @@ INSERT INTO `data_pasien` (`id_pasien`, `nama_pasien`, `tempat_lahir_pasien`, `t
 -- Table structure for table `data_penjaringan`
 --
 
-CREATE TABLE IF NOT EXISTS `data_penjaringan` (
+CREATE TABLE `data_penjaringan` (
   `id_pasien` int(11) NOT NULL,
   `id_penjaringan` int(11) NOT NULL,
   `berat_badan` int(3) NOT NULL,
@@ -95,42 +112,13 @@ CREATE TABLE IF NOT EXISTS `data_penjaringan` (
   `kesehatan_umum` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `data_rm`
+-- Dumping data for table `data_penjaringan`
 --
 
-CREATE TABLE IF NOT EXISTS `data_rm` (
-  `id_rm` int(11) NOT NULL,
-  `id_tindakan` int(11) NOT NULL,
-  `id_petugas` int(11) NOT NULL,
-  `id_pasien` int(11) NOT NULL,
-  `id_obat` int(11) NOT NULL,
-  `tgl_pemeriksaan` date NOT NULL,
-  `keluhan` varchar(100) NOT NULL,
-  `diagnosis` varchar(100) NOT NULL,
-  `tindakan` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `data_tindakan`
---
-
-CREATE TABLE IF NOT EXISTS `data_tindakan` (
-  `id_tindakan` int(11) NOT NULL,
-  `nama_tindakan` varchar(50) NOT NULL,
-  `keterangan` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `data_tindakan`
---
-
-INSERT INTO `data_tindakan` (`id_tindakan`, `nama_tindakan`, `keterangan`) VALUES
-(1, 'Obat', 'Hanya diberikan obat sesuai keluhan');
+INSERT INTO `data_penjaringan` (`id_pasien`, `id_penjaringan`, `berat_badan`, `tinggi_badan`, `td_sistole`, `td_diastole`, `gol_darah`, `hb_darah`, `visus`, `lila`, `haid_pertama`, `kesehatan_gigi`, `status_gizi`, `kesehatan_umum`) VALUES
+(10002, 1, 40, 140, 120, 80, 'B', '40', 'ba', '90', '0000-00-00', 'normal', 'Normal', 'Normal'),
+(10001, 2, 60, 168, 120, 80, 'B', 'ba', 'ba', '90', '0000-00-00', 'bagus', 'Normal', 'Normal');
 
 -- --------------------------------------------------------
 
@@ -138,12 +126,12 @@ INSERT INTO `data_tindakan` (`id_tindakan`, `nama_tindakan`, `keterangan`) VALUE
 -- Table structure for table `login`
 --
 
-CREATE TABLE IF NOT EXISTS `login` (
+CREATE TABLE `login` (
   `id_user` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `level` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `login`
@@ -157,6 +145,12 @@ INSERT INTO `login` (`id_user`, `username`, `password`, `level`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `data_kunjungan`
+--
+ALTER TABLE `data_kunjungan`
+  ADD PRIMARY KEY (`id_kunjungan`);
 
 --
 -- Indexes for table `data_obat`
@@ -177,18 +171,6 @@ ALTER TABLE `data_penjaringan`
   ADD PRIMARY KEY (`id_penjaringan`);
 
 --
--- Indexes for table `data_rm`
---
-ALTER TABLE `data_rm`
-  ADD PRIMARY KEY (`id_rm`);
-
---
--- Indexes for table `data_tindakan`
---
-ALTER TABLE `data_tindakan`
-  ADD PRIMARY KEY (`id_tindakan`);
-
---
 -- Indexes for table `login`
 --
 ALTER TABLE `login`
@@ -199,35 +181,36 @@ ALTER TABLE `login`
 --
 
 --
+-- AUTO_INCREMENT for table `data_kunjungan`
+--
+ALTER TABLE `data_kunjungan`
+  MODIFY `id_kunjungan` int(50) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `data_obat`
 --
 ALTER TABLE `data_obat`
-  MODIFY `id_obat` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id_obat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `data_pasien`
 --
 ALTER TABLE `data_pasien`
-  MODIFY `id_pasien` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id_pasien` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10005;
+
 --
 -- AUTO_INCREMENT for table `data_penjaringan`
 --
 ALTER TABLE `data_penjaringan`
-  MODIFY `id_penjaringan` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `data_rm`
---
-ALTER TABLE `data_rm`
-  MODIFY `id_rm` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `data_tindakan`
---
-ALTER TABLE `data_tindakan`
-  MODIFY `id_tindakan` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id_penjaringan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
